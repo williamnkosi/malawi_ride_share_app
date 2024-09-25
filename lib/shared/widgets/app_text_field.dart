6dfcf042f0eb020d4a4e4ef5ec68618995872b03
William Nkosi <william.dev.acc@gmail.com>
2024-09-25 19:27:57 -0400

    created the signup user page

--- lib/shared/widgets/app_text_field.dart
+++ lib/shared/widgets/app_text_field.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppTextFieldWidget extends StatelessWidget {
   final String hintText;
-  AppTextFieldWidget({super.key, required this.hintText});
+  final bool obscureText;
+  const AppTextFieldWidget(
+      {super.key, required this.hintText, this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      obscureText: obscureText,
       decoration: InputDecoration(
         enabledBorder: const UnderlineInputBorder(
           borderSide: BorderSide(color: Colors.black),

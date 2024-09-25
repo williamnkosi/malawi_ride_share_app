e46925fd7bfe33fa74b2d0052da884ed155237fd
William Nkosi <william.dev.acc@gmail.com>
2024-09-25 19:58:06 -0400

    setup the signup driver page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/pages/signup_driver_page/signup_driver_page.dart';
 import 'package:malawi_ride_share_app/pages/signup_user_driver_page.dart/signup_user_driver_page.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignupUserDriverPage(),
+      home: const SignupDriverPage(),
     );
   }
 }
--- lib/pages/signup_user_page/signup_user_page.dart
+++ lib/pages/signup_user_page/signup_user_page.dart
@@ -10,25 +10,28 @@ class SignupUserPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const AppTextFieldWidget(hintText: 'First Name'),
-            const AppTextFieldWidget(hintText: 'last Name'),
-            const AppTextFieldWidget(hintText: 'Email'),
-            const AppTextFieldWidget(
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            const AppTextFieldWidget(
-                hintText: 'Confirm Password', obscureText: true),
-            const SizedBox(height: 32),
-            SizedBox(
-                width: double.infinity,
-                child:
-                    AppOutlineButton(buttonText: 'Continue', onPressed: () {})),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const AppTextFieldWidget(hintText: 'First Name'),
+              const AppTextFieldWidget(hintText: 'last Name'),
+              const AppTextFieldWidget(hintText: 'Email'),
+              const AppTextFieldWidget(hintText: 'Phone Number'),
+              const AppTextFieldWidget(
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              const AppTextFieldWidget(
+                  hintText: 'Confirm Password', obscureText: true),
+              const SizedBox(height: 32),
+              SizedBox(
+                  width: double.infinity,
+                  child: AppOutlineButton(
+                      buttonText: 'Continue', onPressed: () {})),
+            ],
+          ),
         ),
       ),
     );

8b9fdc99054ab0aed7cb8e8716ef1ad6cca296ce
William Nkosi <william.dev.acc@gmail.com>
2024-09-26 00:15:27 -0400

    page-setup-activity-detail-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/pages/activity_detail_page/activity_detail_page.dart';
 import 'package:malawi_ride_share_app/pages/activity_page/activity_page.dart';
 import 'package:malawi_ride_share_app/pages/signup_driver_page/signup_driver_page.dart';
 import 'package:malawi_ride_share_app/pages/signup_user_driver_page.dart/signup_user_driver_page.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ActivityPage(),
+      home: const ActivityDetailPage(),
     );
   }
 }

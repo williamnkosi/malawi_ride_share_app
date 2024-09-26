98c80323d91184bb92830f6a4fb0072064b49ef9
William Nkosi <william.dev.acc@gmail.com>
2024-09-26 17:58:40 -0400

    Added the bottom naivagtion bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/bottom_navigation_bar.dart';
 import 'package:malawi_ride_share_app/pages/account_page/account_page.dart';
-import 'package:malawi_ride_share_app/pages/activity_detail_page/activity_detail_page.dart';
-import 'package:malawi_ride_share_app/pages/activity_page/activity_page.dart';
-import 'package:malawi_ride_share_app/pages/signup_driver_page/signup_driver_page.dart';
-import 'package:malawi_ride_share_app/pages/signup_user_driver_page.dart/signup_user_driver_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,25 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AccountPage(),
+      home: AppBottomNavigationBar(),
     );
   }
 }

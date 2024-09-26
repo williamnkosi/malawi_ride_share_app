64675ceef3e104aeeb8a3a36cd234044b26f979c
Mandla William Nkosi <16968612+williamnkosi@users.noreply.github.com>
2024-09-26 18:22:36 -0400

    Added the bottom naivagtion bar (#8)

---
+++ lib/bottom_navigation_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/pages/HomePage.dart';
+import 'package:malawi_ride_share_app/pages/account_page/account_page.dart';
+import 'package:malawi_ride_share_app/pages/activity_page/activity_page.dart';
+
+class AppBottomNavigationBar extends StatefulWidget {
+  const AppBottomNavigationBar({super.key});
+
+  @override
+  State<AppBottomNavigationBar> createState() => _AppBottomNavigationBarState();
+}
+
+class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
+  int _selectedIndex = 0;
+  Widget _generatePage() {
+    switch (_selectedIndex) {
+      case 0:
+        return const HomePage();
+      case 1:
+        return const ActivityPage();
+      case 2:
+        return const AccountPage();
+    }
+    return const HomePage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _generatePage(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (value) => setState(() => _selectedIndex = value),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_activity_rounded),
+            label: 'Activity',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          )
+        ],
+      ),
+    );
+  }
+}

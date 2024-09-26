374599cb8cb9c315546094ede64a424f9f88c46d
William Nkosi <william.dev.acc@gmail.com>
2024-09-25 23:54:00 -0400

    added more components to the Activity page

--- lib/pages/activity_page/activity_page.dart
+++ lib/pages/activity_page/activity_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/pages/activity_page/widgets/activity_tile.dart';
 
 class ActivityPage extends StatelessWidget {
   const ActivityPage({super.key});
@@ -7,20 +8,56 @@ class ActivityPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Activity Page'),
+        title: const Text(
+          'Activity Page',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Current Activity',
-              ),
-              SizedBox(height: 16),
-              Text('Past Activities'),
-            ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Current Activity',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ActivityTile(
+                  destination: 'Lilongwe',
+                  origin: 'Blantyre',
+                  date: DateTime.now(),
+                  prices: 'MK 5000',
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Past Activities',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) => ActivityTile(
+                    destination: 'Lilongwe',
+                    origin: 'Blantyre',
+                    date: DateTime.now(),
+                    prices: 'MK 5000',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),

9da023946d63f2c962d7be2606689a01c0d806bc
William Nkosi <william.dev.acc@gmail.com>
2024-09-30 22:46:58 -0400

    setup flavors for ios

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  final String flavor;
+  const App({super.key, required this.flavor});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

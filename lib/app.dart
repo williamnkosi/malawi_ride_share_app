bf8bcd54414c843a53b78859608cb96f73e77cfc
William Nkosi <william.dev.acc@gmail.com>
2024-10-17 00:00:03 -0400

    Changes to app entry point

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_ride_share_app/bottom_navigation_bar.dart';
 
 class App extends StatelessWidget {
   final String flavor;
   const App({super.key, required this.flavor});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AppBottomNavigationBar(),
+    );
   }
 }

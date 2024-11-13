605ff3522f3bc98c33ffbc598d45b06cd7a283a3
Mandla William Nkosi <16968612+williamnkosi@users.noreply.github.com>
2024-11-13 18:00:46 -0500

    Added the dotenv package (#11)

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:malawi_ride_share_app/app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const App(
     flavor: 'Development',
   ));

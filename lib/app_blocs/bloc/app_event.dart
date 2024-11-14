86cb3ea945995d4b1c7b8affb2a353715425175a
William Nkosi <william.dev.acc@gmail.com>
2024-11-13 23:20:16 -0500

    Created the App state bloc

---
+++ lib/app_blocs/bloc/app_event.dart
@@ -0,0 +1,18 @@
+part of 'app_bloc.dart';
+
+sealed class AppEvent extends Equatable {
+  const AppEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class Login extends AppEvent {
+  const Login();
+}
+
+final class SignOut extends AppEvent {
+  const SignOut();
+}
+
+final class RequestLocationPermission extends AppEvent {}

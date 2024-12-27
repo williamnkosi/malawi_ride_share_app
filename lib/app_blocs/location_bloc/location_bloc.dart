4621718ef393fbde2c7b98d5a640c84c6aa59832
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2024-12-27 00:46:27 -0500

    Added logic to the LocationBloc

--- lib/app_blocs/location_bloc/location_bloc.dart
+++ lib/app_blocs/location_bloc/location_bloc.dart
@@ -1,17 +1,82 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:equatable/equatable.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
+part 'location_bloc.freezed.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
-  LocationBloc() : super(LocationInitial()) {
-    on<LocationEvent>(_onGetLocation);
-    on<LocationEvent>(_onSendLocation);
+  LocationBloc() : super(const LocationState()) {
+    on<LocationEventInitial>(_onLocationEventInitial);
+    on<LocationEventTrackLocation>(_onTrackLocation);
+    on<LocationEventStartTracking>(_onStartTracking);
+    on<LocationEventStopTracking>(_onStopTracking);
   }
 
-  _onGetLocation(event, emit) {}
-  _onSendLocation(event, emit) {}
+  _onLocationEventInitial(LocationEvent event, emit) async {
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    try {
+      _serviceEnabled = await location.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          return;
+        }
+      }
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          return;
+        }
+      }
+      LocationData _locationData = await location.getLocation();
+ var locationStream =location.onLocationChanged.listen((LocationData currentLocation) {
+        add(LocationEvent.locationEventTrackLocation(
+            location:
+                LatLng(currentLocation.latitude!, currentLocation.longitude!)));
+      });
+      emit(state.copyWith(
+        locationStream: locationStream,
+          isLocationPremissionEnabled: true,
+          currentLcoation: _locationData,
+          coordinates:
+              LatLng(_locationData.latitude!, _locationData.longitude!)));
+
+     
+      
+    } catch (e) {}
+  }
+
+  _onTrackLocation(LocationEventTrackLocation event, emit) {
+    emit(state.copyWith(coordinates: event.location));
+  }
+
+
+  _onStartTracking(LocationEventStartTracking event, emit) {
+     Location location = Location();
+     var locationStream =location.onLocationChanged.listen((LocationData currentLocation) {
+        add(LocationEvent.locationEventTrackLocation(
+            location:
+                LatLng(currentLocation.latitude!, currentLocation.longitude!)));
+      });
+    emit(state.copyWith(locationStream: locationStream));
+
+  }
+
+  _onStopTracking(LocationEventStopTracking event, emit) {
+    state.locationStream!.cancel();
+    emit(state.copyWith(locationStream: null));
+  }
 }
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:malawi_ride_share_app/app_blocs/location_bloc/location_bloc.dart';
 import 'package:malawi_ride_share_app/pages/home_page/bloc/home_page_bloc.dart';
-import 'package:web_socket/web_socket.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomePageBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => HomePageBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LocationBloc()..add( const LocationEvent.locationEventInitial()),
+          lazy: false,
+        ),
+      ],
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Home Page'),
@@ -23,123 +31,34 @@ class HomePage extends StatelessWidget {
 
 class View extends StatelessWidget {
   const View({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.of<HomePageBloc>(context).state.currentLocation == null
-        ? const Center(
-            child: CircularProgressIndicator(),
-          )
-        : BlocBuilder<HomePageBloc, HomePageState1>(
-            builder: (context, state) {
-              return GoogleMap(
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(
-                      37.4223, -122.0848), // Coordinates for San Francisco
-                  zoom: 13,
-                ),
-                markers: {
-                  Marker(
-                      markerId: const MarkerId('currentLcoation'),
-                      position: state.currentLocation!,
-                      icon: BitmapDescriptor.defaultMarker),
-                },
-                // polylines: Set<Polyline>.of(_polylines.values),
-              );
+    return 
+      BlocBuilder<LocationBloc, LocationState>(
+      
+        builder: (context, state) {
+          if(state.coordinates == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          
+          return GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: state.coordinates!,
+              zoom: 14,
+            ),
+            markers: {
+              Marker(
+                markerId: MarkerId('currentLocation'),
+                position: state.coordinates!,
+              ),
             },
           );
-  }
-}
-
-class WebSocketExample extends StatefulWidget {
-  const WebSocketExample({super.key});
-
-  @override
-  State<WebSocketExample> createState() => _WebSocketExampleState();
-}
-
-class _WebSocketExampleState extends State<WebSocketExample> {
-  late WebSocket webSocket;
-  final TextEditingController _controller = TextEditingController();
-  List<String> messages = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _connectToWebSocket();
-  }
-
-  // Connect to the WebSocket server
-  void _connectToWebSocket() async {
-    webSocket = await WebSocket.connect(Uri.parse(
-        'ws://172.0.0.2:8080/ws/drivers')); // Replace with your WebSocket server URL
-    webSocket.events.listen((e) {
-      if (e is TextDataReceived) {
-        setState(() {
-          messages.add('Server: ${e.text}');
-        });
-      } else if (e is CloseReceived) {
-        setState(() {
-          messages.add('Connection to server closed: ${e.code} [${e.reason}]');
-        });
-      } else {
-        print('Unknown event: $e');
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    webSocket.close(); // Close WebSocket connection
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _sendMessage() {
-    // if (_controller.text.isNotEmpty) {
-    //   webSocket.add(_controller.text); // Send a message
-    //   setState(() {
-    //     messages.add('You: ${_controller.text}');
-    //   });
-    //   _controller.clear();
-    // }
-    print("tewst");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('WebSocket Example')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return Text(messages[index]);
-                },
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter message'),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: _sendMessage,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+        },
+      );
+    
+          
   }
 }

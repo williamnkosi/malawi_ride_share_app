f72f17167e322459df961498c5e448d33b6698a3
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2024-12-27 02:14:50 -0500

    added logic to the location_repository

--- lib/app_blocs/location_bloc/location_bloc.dart
+++ lib/app_blocs/location_bloc/location_bloc.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/rendering.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:malawi_ride_share_app/repository/location_repository.dart';
 import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:equatable/equatable.dart';
@@ -15,6 +17,7 @@ part 'location_state.dart';
 part 'location_bloc.freezed.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  final LocationRepository locationRepository = GetIt.instance<LocationRepository>();
   LocationBloc() : super(const LocationState()) {
     on<LocationEventInitial>(_onLocationEventInitial);
     on<LocationEventTrackLocation>(_onTrackLocation);
@@ -54,26 +57,17 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
   }
 
   _onTrackLocation(LocationEventTrackLocation event, emit) {
-    print(event.location);
     emit(state.copyWith(coordinates: event.location));
   }
 
   _onStartTracking(LocationEventStartTracking event, emit) {
     try {
-      final uri = Uri.parse('ws://10.0.2.2:8080/ws/drivers');
-      final timeout = Duration(seconds: 10);
-      final header = {"Authorization": "testing", "DriverId": "1"};
-      var _socket = WebSocket(uri, headers: header, timeout: timeout);
+
       Location location = Location();
       var locationStream =
           location.onLocationChanged.listen((LocationData currentLocation) {
-        Map<String, dynamic> locationData = {
-          'Latitude': currentLocation.latitude,
-          'Longitude': currentLocation.longitude,
-        };
 
-        var data = jsonEncode(locationData);
-        _socket.send(data);
+        locationRepository.sendLocation(locationData: currentLocation);
         add(LocationEvent.locationEventTrackLocation(
             location:
                 LatLng(currentLocation.latitude!, currentLocation.longitude!)));

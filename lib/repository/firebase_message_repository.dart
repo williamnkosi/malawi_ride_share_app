b4f7d7b6e992c9fecc2e507eb813f8f2fa3a1036
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-01-10 17:25:42 -0500

    Added code to listen to inapp messages

--- lib/repository/firebase_message_repository.dart
+++ lib/repository/firebase_message_repository.dart
@@ -9,6 +9,14 @@ class FirebaseMessageRepository {
       await _firebaseMessaging.requestPermission();
       _firebaseMessaging.getToken().then((token) {
         print('Token: $token');
+
+        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+          if (message.notification != null) {
+            print("Notification Message: ${message.notification!.title}");
+          } else if (message.data.isNotEmpty) {
+            print("Data Message: ${message.data}");
+          }
+        });
       });
     } catch (e) {
       print("-----");

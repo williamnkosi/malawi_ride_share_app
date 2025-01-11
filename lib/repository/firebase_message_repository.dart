ef35c5a5d17e158706d612676df441a3c5d001af
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-01-10 23:56:42 -0500

    Removed websocket code

--- lib/repository/firebase_message_repository.dart
+++ lib/repository/firebase_message_repository.dart
@@ -1,26 +1,21 @@
+import 'dart:async';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class FirebaseMessageRepository {
   late FirebaseMessaging _firebaseMessaging;
 
-  Future<void> initNotifications() async {
+  Future<StreamSubscription<RemoteMessage>> initNotifications() async {
     try {
       _firebaseMessaging = FirebaseMessaging.instance;
       await _firebaseMessaging.requestPermission();
-      _firebaseMessaging.getToken().then((token) {
-        print('Token: $token');
-
-        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-          if (message.notification != null) {
-            print("Notification Message: ${message.notification!.title}");
-          } else if (message.data.isNotEmpty) {
-            print("Data Message: ${message.data}");
-          }
-        });
+      return FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print('Message received: ${message.messageId}');
       });
     } catch (e) {
       print("-----");
       print(e);
+      throw Exception('Failed to initialize notifications');
     }
   }
 }

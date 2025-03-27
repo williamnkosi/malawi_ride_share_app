1ba8767e36da1461103b87615a67cc835add3f84
Mandla William Nkosi <16968612+williamnkosi@users.noreply.github.com>
2025-03-26 22:52:44 -0400

    Setup error loggin (#19)
    * Added the loggin package
    
    * setup loggin and more detailed error handling in the LocationRepository
    
    * Added loggin and exceptions to the firebaseRepository
    
    * Added loggin and exceptions to the FirebaseMessaginRepository
    
    ---------
    
    Co-authored-by: William Nkosi <williamnkosi@Williams-MacBook-Pro.local>

--- lib/repository/firebase_message_repository.dart
+++ lib/repository/firebase_message_repository.dart
@@ -1,22 +1,25 @@
 import 'dart:async';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:malawi_ride_share_app/shared/widgets/app_bottom_sheet.dart';
+import 'package:logging/logging.dart';
+
+final _name = 'FirebaseMessageRepository';
 
 class FirebaseMessageRepository {
   late FirebaseMessaging _firebaseMessaging;
-
+  final Logger logger = Logger(_name);
   Future<Stream<RemoteMessage>> initNotifications() async {
     try {
       _firebaseMessaging = FirebaseMessaging.instance;
       await _firebaseMessaging.requestPermission();
       _firebaseMessaging.getToken().then((value) {
-        print('Token: $value');
+        logger.info(' $_name - Firebase token: $value /n');
       });
+
       return FirebaseMessaging.onMessage;
     } catch (e) {
-      print("-----");
-      print(e);
+      logger.severe('$_name - Error initializing Firebase notifications', e,
+          StackTrace.current);
       throw Exception('Failed to initialize notifications');
     }
   }

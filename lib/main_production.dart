8ee298a752a421ea029fa645750af58391357c3e
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-03-26 21:43:26 -0400

    Added the loggin package

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 import 'package:malawi_ride_share_app/app.dart';
 import 'package:malawi_ride_share_app/services/locator.dart';
 
+final Logger logger = Logger("Malawi Ride Share App");
+
 void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    log(rec.message,
+        time: rec.time,
+        level: rec.level.value,
+        name: rec.loggerName,
+        error: rec.error,
+        stackTrace: rec.stackTrace);
+  });
   // Ensure that the Flutter framework is initialized
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();

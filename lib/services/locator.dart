d5ea144d3d7843192a75d50a0e677db56a23013c
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-05-24 13:57:10 -0400

    Created the DriverTripRepository

--- lib/services/locator.dart
+++ lib/services/locator.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:malawi_ride_share_app/app_blocs/driver_trip_bloc/driver_trip_repository.dart';
 import 'package:malawi_ride_share_app/repository/auth_repository.dart';
 import 'package:malawi_ride_share_app/repository/firebase_message_repository.dart';
 import 'package:malawi_ride_share_app/repository/firebase_repository.dart';
@@ -12,4 +13,5 @@ void setupGetIt() async {
   getIt.registerLazySingleton<FirebaseMessageRepository>(
       () => FirebaseMessageRepository());
   getIt.registerSingleton<LocationRepository>(LocationRepository());
+  getIt.registerSingleton<DriverTripRepository>(DriverTripRepository());
 }

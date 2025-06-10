0a5f1c29092e63ba744f64b5da64044b1e4a5270
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-06-10 18:58:21 -0400

    Added button for rider/driver, added new state

--- lib/app_blocs/app_bloc/app_state.dart
+++ lib/app_blocs/app_bloc/app_state.dart
@@ -1,10 +1,13 @@
 part of 'app_bloc.dart';
 
+enum UserType { driver, rider }
+
 @freezed
 class AppState with _$AppState {
   const factory AppState(
       {@Default(false) bool isLocationPremissionEnabled,
       @Default(false) bool isLoggedIn,
       RemoteMessage? message,
+      UserType? userType,
       Stream<RemoteMessage>? inAppMessageSubscriptioin}) = _AppState;
 }

ab9fc31ad30b94471a3d8661d5fee3596e71d9a4
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-06-21 16:52:25 -0400

    Created the SearchRiderBottomSheet

--- lib/pages/home_page/rider_home_page/rider_home_page.dart
+++ lib/pages/home_page/rider_home_page/rider_home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:malawi_ride_share_app/app_blocs/location_bloc/location_bloc.dart';
 import 'package:malawi_ride_share_app/app_blocs/rider_trip_bloc/rider_trip_bloc.dart';
+import 'package:malawi_ride_share_app/pages/home_page/rider_home_page/search_rider_bottom_sheet.dart';
 import 'package:malawi_ride_share_app/shared/widgets/app_bottom_sheet.dart';
 
 class RiderHomePage extends StatelessWidget {
@@ -36,7 +37,9 @@ class RiderHomePage extends StatelessWidget {
               ),
               SizedBox(width: 10),
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  SearchRiderBottomSheet(context: context);
+                },
                 tooltip: 'Increment',
                 child: Icon(Icons.remove_done),
               ),

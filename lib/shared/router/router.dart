a852a12f9338f2889c33a47ce6ba50e86ef4c132
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-07-21 21:49:57 -0400

    Updated the UserSignUp page

--- lib/shared/router/router.dart
+++ lib/shared/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:malawi_ride_share_app/bottom_navigation_bar.dart';
-import 'package:malawi_ride_share_app/pages/auth_signup_page/auth_signup_page.dart';
 import 'package:malawi_ride_share_app/pages/login_page/login_page.dart';
+import 'package:malawi_ride_share_app/pages/signup_user_page/signup_user_page.dart';
 
 List<RouteBase> appRoutes = [
   GoRoute(
@@ -10,7 +10,7 @@ List<RouteBase> appRoutes = [
   ),
   GoRoute(
     path: AppRoutes.authSignUpPage,
-    builder: (context, state) => AuthSignupPage(),
+    builder: (context, state) => SignupUserPage(),
   ),
   GoRoute(
     path: AppRoutes.homePage,

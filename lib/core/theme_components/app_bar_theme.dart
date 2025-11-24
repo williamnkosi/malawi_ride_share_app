04edcadd2e0a7a71f6a17bb409732a21801463c4
Mandla William Nkosi <16968612+williamnkosi@users.noreply.github.com>
2025-11-24 16:00:59 -0500

    Setup theme UI structure (#25)
    * Added theme folder and files
    
    * Added Theme data to files
    
    * Setup colors and themes
    
    * Setup UI tokens
    
    * Added TextTheme
    
    * Added theme to app
    
    * Removed uneeded code
    
    * Added appElevation and bottomSheetTheme
    
    * Created themes for buttons in app
    
    * Added the InputDecorationTheme
    
    * Refactored code
    
    * Create the app_bar_theme component
    
    * Create the DialogTheme and SnackBarTheme
    
    * Refactored code
    
    * removed uneeded code
    
    ---------
    
    Co-authored-by: William Nkosi <williamnkosi@mac.mynetworksettings.com>

---
+++ lib/core/theme_components/app_bar_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:malawi_ride_share_app/core/tokens/app_colors.dart';
+import 'package:malawi_ride_share_app/core/tokens/app_elevation.dart';
+import 'package:malawi_ride_share_app/core/tokens/app_typography_token.dart';
+
+AppBarTheme buildAppBarTheme(
+    AppElevation elevation, AppTypographyToken typographyToken) {
+  return AppBarTheme(
+    backgroundColor: AppColors.surface,
+    foregroundColor: AppColors.onSurface,
+    elevation: elevation.low,
+    centerTitle: false,
+    titleTextStyle: TextStyle(
+      fontSize: typographyToken.titleLarge,
+      fontWeight: FontWeight.w600,
+      color: AppColors.onSurface,
+    ),
+    iconTheme: IconThemeData(
+      color: AppColors.gray80,
+      size: 24,
+    ),
+    actionsIconTheme: IconThemeData(
+      color: AppColors.gray80,
+      size: 24,
+    ),
+    systemOverlayStyle: SystemUiOverlayStyle.dark,
+  );
+}

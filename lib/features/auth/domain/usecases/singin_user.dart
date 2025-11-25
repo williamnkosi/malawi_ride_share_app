40540686173e56df409c16e3744267060d9578ab
William Nkosi <williamnkosi@mac.mynetworksettings.com>
2025-11-25 17:28:03 -0500

    Added usecase

---
+++ lib/features/auth/domain/usecases/singin_user.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:malawi_ride_share_app/core/usecase/usecase.dart';
+import 'package:malawi_ride_share_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:malawi_ride_share_app/features/auth/domain/usecases/email_password_params.dart';
+
+class SignInUser implements UseCase<UserCredential, EmailPasswordParams> {
+  final AuthRepository authRepository;
+
+  SignInUser(this.authRepository);
+
+  @override
+  Future<UserCredential> call(EmailPasswordParams params) async {
+    return await authRepository.loginInUserWithEmailAndPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}

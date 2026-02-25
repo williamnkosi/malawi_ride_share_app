0663fcd63986bff4f56652160981ab8f6aa0b86e
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2026-02-24 22:18:18 -0500

    setup logic to draw polyline to the pickupLocation

--- lib/features/driver/presentation/pages/driver_active_trip_page/driver_active_trip.dart
+++ lib/features/driver/presentation/pages/driver_active_trip_page/driver_active_trip.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:malawi_ride_share_app/features/driver/domain/entity/driver_trip.dart';
+import 'package:malawi_ride_share_app/features/driver/presentation/bloc/driver_trip_bloc/driver_trip_bloc.dart';
 import 'package:malawi_ride_share_app/features/driver/presentation/pages/driver_active_trip_page/widgets/driver_active_trip_info_card.dart';
 import 'package:malawi_ride_share_app/features/driver/presentation/pages/driver_active_trip_page/widgets/driver_active_trip_map_view.dart';
-import 'package:malawi_ride_share_app/features/location/domain/entities/location.dart';
 import 'package:malawi_ride_share_app/features/location/presentation/location_bloc/location_bloc.dart';
 import 'package:malawi_ride_share_app/services/locator.dart';
 
@@ -16,13 +15,16 @@ class DriverActiveTrip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: getIt<LocationBloc>(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: getIt<LocationBloc>()),
+        BlocProvider.value(value: getIt<DriverTripBloc>()),
+      ],
       child: Scaffold(
         appBar: AppBar(title: const Text('Active Trip')),
         body: Column(
           children: [
-            Expanded(child: DriverActiveTripMapView()),
+            DriverActiveTripMapView(),
             DriverActiveTripInfoCard(trip: trip),
           ],
         ),
--- lib/features/driver/presentation/pages/driver_active_trip_page/widgets/driver_active_trip_map_view.dart
+++ lib/features/driver/presentation/pages/driver_active_trip_page/widgets/driver_active_trip_map_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:logging/logging.dart';
 import 'package:malawi_ride_share_app/features/location/domain/entities/location.dart';
 import 'package:malawi_ride_share_app/features/location/presentation/location_bloc/location_bloc.dart';
+import 'package:malawi_ride_share_app/features/driver/presentation/bloc/driver_trip_bloc/driver_trip_bloc.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class DriverActiveTripMapView extends StatelessWidget {
-  const DriverActiveTripMapView({super.key});
+  DriverActiveTripMapView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +19,7 @@ class DriverActiveTripMapView extends StatelessWidget {
               MapView(key: const ValueKey('driver_map'), location: location),
           errors: (striing, location) => Expanded(child: Text('data')),
           orElse: () {
-            return const Expanded(
-              child: Center(child: CircularProgressIndicator()),
-            );
+            return Center(child: CircularProgressIndicator());
           },
         );
       },
@@ -36,13 +37,27 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  final logger = Logger('DriverMap');
   GoogleMapController? _mapController;
   LocationEntity? _lastPosition;
+  Set<Polyline> _polylines = {};
 
   @override
   void initState() {
     super.initState();
     _lastPosition = widget.location;
+    
+    // Read initial state from DriverTripBloc
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final tripState = context.read<DriverTripBloc>().state;
+      tripState.maybeWhen(
+        enRouteToPickup: (activeTrip, routeToPickup) {
+          logger.info('Initial state: enRouteToPickup detected');
+          _updateRoutePolyline(routeToPickup.polyline);
+        },
+        orElse: () {},
+      );
+    });
   }
 
   void _updateCameraPosition(LocationEntity? newPosition) {
@@ -74,40 +89,81 @@ class _MapViewState extends State<MapView> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LocationBloc, LocationState>(
-      listener: (context, state) {
-        state.maybeWhen(
-          active: (currentLocation) {
-            _updateCameraPosition(currentLocation);
+    return Expanded(
+      child: BlocListener<LocationBloc, LocationState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            active: (currentLocation) {
+              _updateCameraPosition(currentLocation);
+            },
+            orElse: () {},
+          );
+        },
+        child: BlocListener<DriverTripBloc, DriverTripState>(
+          listener: (context, state) {
+            logger.info('DriverTripBloc state changed: ${state.runtimeType}');
+            state.maybeWhen(
+              enRouteToPickup: (activeTrip, routeToPickup) {
+                logger.info('enRouteToPickup state detected');
+                _updateRoutePolyline(routeToPickup.polyline);
+              },
+              orElse: () {},
+            );
           },
-
-          orElse: () {},
-        );
-      },
-      child: Expanded(
-        child: GoogleMap(
-          onMapCreated: (GoogleMapController controller) {
-            _mapController = controller;
-            _updateCameraPosition(widget.location);
-          },
-          initialCameraPosition: CameraPosition(
-            target: LatLng(
-              widget.location?.latitude ?? 0,
-              widget.location?.longitude ?? 0,
+          child: GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              _mapController = controller;
+              _updateCameraPosition(widget.location);
+            },
+            initialCameraPosition: CameraPosition(
+              target: LatLng(
+                widget.location?.latitude ?? 0,
+                widget.location?.longitude ?? 0,
+              ),
+              zoom: 15.0,
             ),
-            zoom: 15.0,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            compassEnabled: true,
+            mapToolbarEnabled: false,
+            zoomControlsEnabled: true,
+            mapType: MapType.normal,
+            polylines: _polylines,
+            onTap: (LatLng position) {
+              // TODO: Handle map tap if needed
+            },
           ),
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-          compassEnabled: true,
-          mapToolbarEnabled: false,
-          zoomControlsEnabled: true,
-          mapType: MapType.normal,
-          onTap: (LatLng position) {
-            // TODO: Handle map tap if needed
-          },
         ),
       ),
     );
+  }
+
+  void _updateRoutePolyline(String encodedPolyline) {
+    final logger = Logger('DriverMap');
+    logger.info('DEBUG: Encoded polyline: $encodedPolyline');
+    final polylinePoints = PolylinePoints();
+    final decodedPoints = polylinePoints.decodePolyline(encodedPolyline);
+    logger.info('DEBUG: Decoded points count: ${decodedPoints.length}');
+
+    final polylineCoordinates = <LatLng>[];
+    if (decodedPoints.isNotEmpty) {
+      polylineCoordinates.addAll(
+        decodedPoints.map((point) => LatLng(point.latitude, point.longitude)),
+      );
+    }
+
+    logger.info('DEBUG: Polyline coordinates: ${polylineCoordinates.length}');
+    setState(() {
+      _polylines.clear();
+      _polylines.add(
+        Polyline(
+          polylineId: const PolylineId('route_to_pickup'),
+          color: Colors.blue,
+          points: polylineCoordinates,
+          width: 5,
+        ),
+      );
+      logger.info('DEBUG: Polylines updated, count: ${_polylines.length}');
+    });
   }
 }
